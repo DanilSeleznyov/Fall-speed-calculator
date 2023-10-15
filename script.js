335bd633012ae29f4cd6e32dfee21f3a7625b5f2
DanilSeleznyov <91893687+DanilSeleznyov@users.noreply.github.com>
2023-10-15 15:04:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 let weightUnit = null
 let sizeUnit = null
 let shape = null
+
+if (localStorage.getItem('shape') == undefined &&
+    localStorage.getItem('sizeUnit') == undefined &&
+    localStorage.getItem('weightUnit') == undefined) {
+        localStorage.setItem('shape', JSON.stringify(0))
+        localStorage.setItem('sizeUnit', JSON.stringify(5))
+        localStorage.setItem('weightUnit', JSON.stringify(1))     
+}
